c93e543d196d234cbf8e6b47ea5a6d71ba95a655
Anoop Kumar <anoopisewalia156@gmail.com>
2026-01-02 10:16:21 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,12 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import ShopContextProvider from "./context/ShopContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter} from 'react-router-dom';
+import App from './App.jsx'
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ShopContextProvider>
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </ShopContextProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </StrictMode>
 );
